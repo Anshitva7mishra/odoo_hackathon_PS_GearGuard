7392765560b351814fabb9d01e32cc382f0d234c
Komal <rastogikomal57@gmail.com>
2025-12-27 13:13:21 +0530

    Dashboard stats created

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { connectDB } from "./config/db.js";
+import sequelize, { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import equipmentRoutes from "./routes/equipmentRoutes.js";
 import maintenanceRoutes from "./routes/maintenanceRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 import User from "./models/User.js";
 import Equipment from "./models/Equipment.js";
 import Maintenance from "./models/Maintenance.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
@@ -23,17 +23,36 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
 
-Maintenance.belongsTo(User, { foreignKey: "technicianId" });
-Maintenance.belongsTo(Equipment, { foreignKey: "equipmentId" });
+Maintenance.belongsTo(User, { foreignKey: "technicianId", as: "technician" });
+Maintenance.belongsTo(Equipment, {
+  foreignKey: "equipmentId",
+  as: "equipment",
+});
 User.hasMany(Maintenance, { foreignKey: "technicianId" });
 Equipment.hasMany(Maintenance, { foreignKey: "equipmentId" });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/equipment", equipmentRoutes);
 app.use("/api/maintenance", maintenanceRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    await sequelize.sync({ alter: true });
+    console.log("✅ Database Tables Synced Successfully");
+
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  } catch (error) {
+    console.error("❌ Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
+
